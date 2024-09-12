1bdf6860c7dfcbc3e14fdfae277937fa8cf07ceb
AliRamadhan <aliramdhan4551@gmail.com>
2024-09-12 20:50:34 +0700

    feat : initialize authentication project

---
+++ server/authentication/src/main/java/com/final_project_clinic/authentication/AuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.final_project_clinic.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationApplication.class, args);
+	}
+
+}
