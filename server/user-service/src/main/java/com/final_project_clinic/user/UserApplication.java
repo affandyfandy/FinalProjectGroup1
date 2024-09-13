68c80495900716d6114628a038d23a737bec115f
AliRamadhan <aliramdhan4551@gmail.com>
2024-09-13 13:12:51 +0700

    feat : setup project structure user management service

---
+++ server/user-service/src/main/java/com/final_project_clinic/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.final_project_clinic.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
