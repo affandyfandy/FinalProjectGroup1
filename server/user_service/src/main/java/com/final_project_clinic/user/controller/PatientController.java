29d9b0210fb078fda38144884ad534a051bebf1c
AliRamadhan <aliramdhan4551@gmail.com>
2024-09-16 14:00:01 +0700

    feat : implement audit log into controller

--- server/user_service/src/main/java/com/final_project_clinic/user/controller/PatientController.java
+++ server/user_service/src/main/java/com/final_project_clinic/user/controller/PatientController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import com.final_project_clinic.user.dto.PatientDTO;
@@ -18,7 +19,7 @@ import com.final_project_clinic.user.service.PatientService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/v1/patients")
+@RequestMapping("/api/v1/all/patients")
 public class PatientController {
 
     private final PatientService patientService;
@@ -28,7 +29,21 @@ public class PatientController {
         this.patientService = patientService;
     }
 
+    @GetMapping("/public")
+    public ResponseEntity<Page<PatientShowDTO>> getAllPatientsPublic(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<PatientShowDTO> patients = patientService.findAllPatients(pageable);
+
+        if (patients.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(patients);
+    }
+
     // Get all patients with pagination
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN')")
     @GetMapping
     public ResponseEntity<Page<PatientShowDTO>> getAllPatients(@RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size) {
@@ -43,6 +58,7 @@ public class PatientController {
     }
 
     // Get patient by ID
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<PatientShowDTO> getPatientById(@PathVariable UUID id) {
         PatientShowDTO patient = patientService.findPatientById(id);
@@ -50,6 +66,7 @@ public class PatientController {
     }
 
     // Create new patient
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN')")
     @PostMapping
     public ResponseEntity<PatientDTO> createPatient(@Valid @RequestBody PatientSaveDTO patientSaveDTO) {
         PatientDTO createdPatient = patientService.createPatient(patientSaveDTO);
@@ -57,6 +74,7 @@ public class PatientController {
     }
 
     // Update existing patient
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PatientDTO> updatePatient(
             @PathVariable UUID id,
@@ -66,6 +84,7 @@ public class PatientController {
     }
 
     // Delete patient by ID
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN', 'ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
         patientService.deletePatient(id);
--- server/user_service/src/main/java/com/final_project_clinic/user/controller/UserController.java
+++ server/user_service/src/main/java/com/final_project_clinic/user/controller/UserController.java
@@ -10,12 +10,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/all/users")
 public class UserController {
 
     private final UserService userService;
@@ -25,28 +26,38 @@ public class UserController {
         this.userService = userService;
     }
 
-    // Retrieve a paginated list of users
+    @GetMapping("/public")
+    public ResponseEntity<Page<UserShowDTO>> getAllUsersPublic(Pageable pageable) {
+        Page<UserShowDTO> users = userService.findAllUsers(pageable);
+        return ResponseEntity.ok(users);
+    }
+
+    // Retrieve a paginated list of users (Only SUPERADMIN can view)
+    @PreAuthorize("hasAuthority('SUPERADMIN')")
     @GetMapping
     public ResponseEntity<Page<UserShowDTO>> getAllUsers(Pageable pageable) {
         Page<UserShowDTO> users = userService.findAllUsers(pageable);
         return ResponseEntity.ok(users);
     }
 
-    // Retrieve a user by ID
+    // Retrieve a user by ID (Only SUPERADMIN can view)
+    @PreAuthorize("hasAuthority('SUPERADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UserShowDTO> getUserById(@PathVariable UUID id) {
         UserShowDTO user = userService.findUserById(id);
         return ResponseEntity.ok(user);
     }
 
-    // Create a new user
+    // Create a new user (Only SUPERADMIN can create)
+    @PreAuthorize("hasAuthority('SUPERADMIN')")
     @PostMapping
     public ResponseEntity<UserDTO> createUser(@RequestBody UserSaveDTO userSaveDTO) {
         UserDTO newUser = userService.createUser(userSaveDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
-    // Update an existing user
+    // Update an existing user (Only SUPERADMIN can update)
+    @PreAuthorize("hasAuthority('SUPERADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<UserDTO> updateUser(
             @PathVariable UUID id,
@@ -55,7 +66,8 @@ public class UserController {
         return ResponseEntity.ok(updatedUser);
     }
 
-    // Delete a user by ID
+    // Delete a user by ID (Only SUPERADMIN can delete)
+    @PreAuthorize("hasAuthority('SUPERADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
         userService.deleteUser(id);
